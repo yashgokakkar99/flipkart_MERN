93679c28c34cdc91579decc175a0fdf6d1986873
Yash Gokakkar <yashgokakkar@gmail.com>
2023-05-25 21:52:54 +0530

    react-multi-carousel updated

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import {bannerData} from '../../constants/data';
-import Carousel from 'react-multi-carousel';
-import {styled} from '@mui/material';
+import React from "react";
+import { bannerData } from "../../constants/data";
+import Carousel from "react-multi-carousel";
+import { styled } from "@mui/material";
+import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
-const Image = styled('img')({
-    width:'100%',
-    height:230
-})
+const Image = styled("img")({
+  width: "100%",
+  height: 230,
+});
 
 function Banner() {
   return (
     <Carousel
-        responsive={responsive}
+      responsive={responsive}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+      containerClass="carousel-container"
+      swipeable={false}
+      draggable={false}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={4000}
+      slidesToSlide={1}
+      keyBoardControl={true}
     >
-        {
-            bannerData.map(data=>(
-                <Image src={data.url} alt='banner'/>
-            ))
-        }
+      {bannerData.map((data) => (
+        <Image src={data.url} alt="banner" />
+      ))}
     </Carousel>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,14 +1,22 @@
-import {Fragment} from 'react'
-import NavBar from './NavBar';
-import Banner from './Banner';
+import { Fragment } from "react";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import { styled, Box } from "@mui/material";
+
+const Container = styled(Box)`
+  padding:10px;
+  background:#f2f2f2;
+`;
 
 function Home() {
   return (
     <Fragment>
-      <NavBar/>
-      <Banner/>
+      <NavBar />
+      <Container>
+        <Banner />
+      </Container>
     </Fragment>
-  )
+  );
 }
 
-export default Home
+export default Home;
